8b82009788b95cd76f795b5d724b3a875286db50
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-04 16:28:08 +0200

    Employees.jsx created and changed

---
+++ src/components/Employees.jsx
@@ -0,0 +1,11 @@
+import { Employee } from "./Employee";
+
+export const Employees = ({ employees }) => {
+  return (
+    <div className="employees">
+      {employees.map((emp, index) => {
+        return <Employee key={index} employee={emp} />;
+      })}
+    </div>
+  );
+};
